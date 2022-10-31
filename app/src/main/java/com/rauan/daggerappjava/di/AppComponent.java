440c74b011331572548a117de136ae3fd86ba928
rauantasbolat <rauan98ktl@gmail.com>
2022-10-31 18:26:08 +0600

    second

--- app/src/main/java/com/rauan/daggerappjava/di/AppComponent.java
+++ app/src/main/java/com/rauan/daggerappjava/di/AppComponent.java
@@ -4,11 +4,15 @@ import android.app.Application;
 
 import com.rauan.daggerappjava.BaseApplication;
 
+import javax.inject.Singleton;
+
 import dagger.BindsInstance;
 import dagger.Component;
 import dagger.android.AndroidInjector;
 import dagger.android.support.AndroidSupportInjectionModule;
 
+
+@Singleton
 @Component(
         modules = {
                 AndroidSupportInjectionModule.class,
--- app/src/main/java/com/rauan/daggerappjava/di/AppModule.java
+++ app/src/main/java/com/rauan/daggerappjava/di/AppModule.java
@@ -10,19 +10,23 @@ import com.bumptech.glide.RequestManager;
 import com.bumptech.glide.request.RequestOptions;
 import com.rauan.daggerappjava.R;
 
+import javax.inject.Singleton;
+
 import dagger.Module;
 import dagger.Provides;
 
 @Module
 public class AppModule {
 
+    @Singleton
     @Provides
     static RequestOptions provideRequestOptions() {
           return RequestOptions
                   .placeholderOf(R.drawable.white_background)
                   .error(R.drawable.white_background);
-    };
+    }
 
+    @Singleton
     @Provides
     static RequestManager provideGlideInstance(Application application, RequestOptions requestOptions) {
         return Glide.with(application)
@@ -30,6 +34,7 @@ public class AppModule {
 
     }
 
+    @Singleton
     @Provides
     static Drawable provideAppDrawable(Application application) {
         return ContextCompat.getDrawable(application, R.drawable.logo);
